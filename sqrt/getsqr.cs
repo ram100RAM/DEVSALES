7895a90aadfcd59781952e98a789a1526bf4b883
Zack Viente <zviente@gmail.com>
2019-10-05 10:11:21 -0600

    Get sqrt in c#

---
+++ sqrt/getsqr.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace GetSquare
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter a number to get the square root: ");
+			int n = Int32.Parse(Console.ReadLine());
+			int sqr;
+			if (n >= 0)
+			{
+				sqr = getSqr(n);
+				Console.WriteLine("The square root of {0} is {1}", n, sqr);
+			}
+
+		}
+
+		private static int getSqr(int toSqr)
+		{
+			return (int)Math.Round(Math.Sqrt(toSqr), 2);
+		}
+	}
+}
