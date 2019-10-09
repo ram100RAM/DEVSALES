9faef0dc9d6c3b524cfb7b032324b3037e508171
Thisara Thushan <38062301+thisarathushan1@users.noreply.github.com>
2019-10-09 17:43:21 +0530

    Added a c code
    Added a c code to find the square root of any number.

---
+++ sqrt/sqrt.c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{   float no;
+    printf("Enter the number: ");
+    scanf("%f",&no);
+
+    printf("Square root of %.1f is %.1f",no,sqrt(no));
+    return 0;
+}
