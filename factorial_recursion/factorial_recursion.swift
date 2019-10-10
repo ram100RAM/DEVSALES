0319834d7e31a7608842b471b2b7f9fbf7f9a978
Samriddhi Srivastava <35305134+samriddhi-99@users.noreply.github.com>
2019-10-10 15:16:43 +0530

    Create  factorial_recursion.swift

---
+++ factorial_recursion/factorial_recursion.swift
@@ -0,0 +1,25 @@
+// @@@ get_version: swiftc --version | awk 'NR == 1 {print $4}'
+// @@@ before: swiftc -O fact.swift
+// @@@ instead: ./fact
+// @@@ after: rm fact
+// @@@ is_fast: 1
+let times = 200000000
+let fact_16: Int64 = 20922789888000
+
+func fact(n: Int64) -> Int64 {
+    if n == 1 {
+        return 1
+    }
+    return n * fact(n: n - 1)
+}
+
+func main() {
+    var ok = true
+    for _ in 1...times {
+        ok = ok && fact(n: 16) == fact_16
+    }
+
+    print("Swift finish \(times) -  \(ok ? "ok" : "fail")")
+}
+
+main()
